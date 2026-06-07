57502e3c8d621295d5dd3cb8efa0d798512cfd1c
Md. Shibli Noman <mdshiblisharkar@gmail.com>
2026-06-07 10:45:02 +0600

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> cars;
+
+        for (int i = 0; i < position.size(); i++) {
+            cars.push_back({position[i], speed[i]});
+        }
+
+        sort(cars.rbegin(), cars.rend());
+
+        int fleets = 0;
+        double maxTime = 0;
+
+        for (auto& car : cars) {
+            int pos = car.first;
+            int spd = car.second;
+
+            double time = (double)(target - pos) / spd;
+
+            if (time > maxTime) {
+                fleets++;
+                maxTime = time;
+            }
+        }
+
+        return fleets;
+    }
+};
